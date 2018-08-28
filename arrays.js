8e6fe78ba1a93fb1bedcf600e5fb293c801bfe84
viqi zevic <vicky.tanzil@gmail.com>
2018-08-28 05:03:51 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -17,3 +17,18 @@ function destructivelyAddElementToEndOfArray(array, element) {
   array.push(element)
   return array
 }
+
+function accessElementInArray(array, index) {
+  return array[index]
+}
+
+function destructivelyRemoveElementFromBeginningOfArray(array) {
+  array.shift()
+  return array
+}
+
+function 
+
+
+
+
