d41eebc2b89dea48be71f432295ece6ef179eca3
viqi zevic <vicky.tanzil@gmail.com>
2018-08-28 05:10:14 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -37,7 +37,7 @@ function destructivelyRemoveElementFromEndOfArray(array) {
 }
 
 function removeElementFromEndOfArray(array) {
-  return array.slice(-1)
+  return array.slice(0 array.length - 1)
 }
 
 
