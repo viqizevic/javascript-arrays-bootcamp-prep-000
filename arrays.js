1740554ccfa201a202e6757ef127d790a104ab09
viqi zevic <vicky.tanzil@gmail.com>
2018-08-28 04:59:40 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -3,3 +3,8 @@ var chocolateBars = ['snickers', "hundred grand", "kitkat", "skitlles"]
 function addElementToBeginningOfArray(array, element) {
   return [element, ...array]
 }
+
+function desctructivelyAddElementToBeginningOfArray(array, element) {
+  array.unshift(element)
+  return array
+}
